e63f2fc2f0b99f4821ebe6cb0e800d05bab31cf3
Arghir Eduard <arghir.eduard@yahoo.com>
2025-02-17 11:30:32 +0200

    more changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,42 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import ToggleButton from "./ToggleButton";
 
 function Navbar({ isDarkMode, toggleTheme }) {
   return (
     <nav className="bg-slate-200 dark:bg-zinc-950 shadow-xl mb-5 border-b border-gray-300 dark:border-gray-700">
-      <div className="container ml-6 pr-6 flex items-center">
+      <div className="container md:ml-6 md:pr-6 flex items-center">
         <div className="flex items-center">
-          <Link to="/">
+          <NavLink to="/">
             <img src={logo} alt="Logo" className="h-24 mr-4" />
-          </Link>
+          </NavLink>
         </div>
         <div className="flex-1 flex justify-center md:ml-8">
-          <ul className="flex space-x-6 text-black dark:text-white text-lg">
+          <ul className="flex space-x-6 text-black dark:text-white md:text-lg text-base">
             <li>
-              <Link
+              <NavLink
                 to="/"
                 className="text-black hover:font-bold dark:text-white"
               >
                 Home
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/gallery"
                 className="text-black hover:font-bold dark:text-white"
               >
                 Gallery
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to="/about"
                 className="text-black hover:font-bold dark:text-white"
               >
                 About
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
