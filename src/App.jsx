de2163468218426ae39420281814890622e75f16
Arghir Eduard <arghir.eduard@yahoo.com>
2025-03-25 09:38:06 +0200

    fhfgf

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Gallery from "./components/Gallery";
-import About from "./components/About";
-import PageNotFound from "./components/PageNotFound";
+import LoadingSpinner from "./components/LoadingSpinner";
+// import Home from "./components/Home";
+// import Gallery from "./components/Gallery";
+// import About from "./components/About";
+// import PageNotFound from "./components/PageNotFound";
+
+const Home = lazy(() => import("./components/Home"));
+const Gallery = lazy(() => import("./components/Gallery"));
+const About = lazy(() => import("./components/About"));
+const PageNotFound = lazy(() => import("./components/PageNotFound"));
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark" || savedTheme === null;
+  });
 
   const toggleTheme = () => {
+    const newTheme = !isDarkMode ? "dark" : "light";
     setIsDarkMode(!isDarkMode);
+    localStorage.setItem("theme", newTheme);
   };
 
   useEffect(() => {
@@ -23,15 +34,17 @@ function App() {
 
   return (
     <Router>
-      <div className="min-h-screen bg-slate-200 dark:bg-zinc-950">
-        <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
+      <Suspense fallback={<LoadingSpinner />}>
+        <div className="min-h-screen bg-slate-200 dark:bg-zinc-950">
+          <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+      </Suspense>
     </Router>
   );
 }
---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import "../styles/LoadingSpinner.css";
+
+const LoadingSpinner = () => (
+  <div className="flex justify-center items-center h-screen">
+    <div className="loader"></div>
+  </div>
+);
+
+export default LoadingSpinner;
